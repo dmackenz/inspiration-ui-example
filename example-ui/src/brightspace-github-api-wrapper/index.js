bc6a7cf2bd0f0c3c2c6e228a0d9bbc56baba5af8
Diljot Garcha <Diljot.Garcha@D2L.com>
2019-11-15 14:50:12 -0500

    Refactor

--- example-ui/src/brightspace-github-api-wrapper/index.js
+++
@@ -1,6 +0,0 @@
-const { GitHubService } = require('./GitHubService')
-
-console.log(GitHubService.loginToApp())
-console.log(GitHubService.isUserLoggedIn())
-console.log(GitHubService.getRepoList())
-console.log(GitHubService.getRepoArchiveLink("DiljotSG", "1password-python"))
